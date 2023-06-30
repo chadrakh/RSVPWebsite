7c469081b66d0f38d4894938e6d06d84d8024363
Shak Holondo <mechakh@icloud.com>
2023-06-30 10:26:56 +0100

    Use sizing props in StyledText

--- src/components/common/Texts/StyledText.tsx
+++ src/components/common/Texts/StyledText.tsx
@@ -1,25 +1,47 @@
 import styled from '@emotion/styled';
 import { FC } from "react";
 
-const StyledPrimary = styled.h1`
-  font-size: 6em;
+type TextProps = {
+  bold?: boolean;
+  italics?: boolean;
+  padding?: number;
+  size?: string;
+};
+
+const StyledPrimary = styled.h1<TextProps>`
+  font-size: ${(props) => (props.size ?? "large")};
+  margin-top: ${(props) => props.padding}em;
+  font-style: ${(props) => (props.italics ? "italic" : "normal")};
+  font-weight: ${(props) => (props.bold ? "bold" : "normal")};
   font-family: "Tangerine", sans-serif;
   font-weight: normal;
   margin-bottom: 0.5em;
 `;
 
-const StyledSecondary = styled.h2`
-  font-size: 3em;
+const StyledSecondary = styled.h2<TextProps>`
+  font-size: ${(props) => (props.size ?? "large")};
+  margin-top: ${(props) => props.padding}em;
+  font-style: ${(props) => (props.italics ? "italic" : "normal")};
+  font-weight: ${(props) => (props.bold ? "bold" : "normal")};
   font-family: "Tangerine", sans-serif;
   margin-bottom: 0.5em;
 `;
 
-const StyledBody = styled.p<{ bold?: boolean, italics?: boolean, padding?: number, size?: string }>`
+const StyledBody = styled.p<TextProps>`
   font-size: ${(props) => (props.size ?? "large")};
   margin-top: ${(props) => props.padding}em;
   font-style: ${(props) => (props.italics ? "italic" : "normal")};
   font-weight: ${(props) => (props.bold ? "bold" : "normal")};
 `;
+
+type Props = {
+  children: string;
+  type?: "header" | "subheader" | "body";
+  bold?: boolean;
+  italics?: boolean;
+  padding?: number;
+  size?: "xx-small" | "x-small" | "small" | "medium" | "large" | "x-large" | "xx-large";
+};
 
 const StyledText: FC<Props> = ({ type, children, bold, italics, padding, size }) => {
   return (
@@ -29,19 +51,18 @@ const StyledText: FC<Props> = ({ type, children, bold, italics, padding, size })
           {children}
         </StyledBody>
       )}
-      {type === "header" && <StyledPrimary>{children}</StyledPrimary>}
-      {type === "subheader" && <StyledSecondary>{children}</StyledSecondary>}
+      
+      {type === "header" && 
+      <StyledPrimary bold={bold} italics={italics} padding={padding} size={size}>
+        {children}
+      </StyledPrimary>}
+
+      {type === "subheader" &&
+       <StyledSecondary bold={bold} italics={italics} padding={padding} size={size}>
+        {children}
+      </StyledSecondary>}
     </>
   );
-};
-
-type Props = {
-  children: string;
-  type?: "header" | "subheader" | "body";
-  bold?: boolean;
-  italics?: boolean;
-  padding?: number;
-  size?: "xx-small" | "x-small" | "small" | "medium" | "large" | "x-large" | "xx-large";
 };
 
 export default StyledText;
